588a198252892780bce53eefb558328b59e29f0a
Mohammad <sharifnewaz1996@gmail.com>
2021-11-11 12:49:47 -0800

    remove login/.js file

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-let { getLogin } = require('../controller/loginController')
-
-
-/* GET home page. */
-router.get('/', getLogin);
-
-module.exports = router;
