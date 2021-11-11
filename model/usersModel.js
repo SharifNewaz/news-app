9c94dc158d18ef82c5d25e4b899f27da6730550d
Mohammad <sharifnewaz1996@gmail.com>
2021-11-11 12:47:56 -0800

    change user DB key names

--- model/usersModel.js
+++ model/usersModel.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name: { type: String, require: true, trim: true, min: 2 },
-    email: { type: String, required: true, trim: true, match: [/(^[a-zA-Z]{1,})((\d){1,}|[\_|\.|\-][a-zA-Z]{1,})?\@([a-zA-Z]{1,})\-?([a-zA-Z]{1,})?\.([a-zA-Z]{2,3})$/, "Please enter a correct formatted email."] },
-    password: { type: String, required: true, trim: true, min: 8, max: 15 },
-    articles: { type: mongoose.Schema.Types.ObjectId, ref: "Article" }
+    uname: { type: String, require: true, trim: true, min: 2 },
+    uemail: { type: String, required: true, trim: true, match: [/(^[a-zA-Z]{1,})((\d){1,}|[\_|\.|\-][a-zA-Z]{1,})?\@([a-zA-Z]{1,})\-?([a-zA-Z]{1,})?\.([a-zA-Z]{2,3})$/, "Please enter a correct formatted email."] },
+    upassword: { type: String, required: true, trim: true, min: 8, max: 15 },
+    uarticles: { type: mongoose.Schema.Types.ObjectId, ref: "Article" }
 });
 
 module.exports = mongoose.model("User", userSchema);
