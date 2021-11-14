9ec0855268133090994b381aada0578f8f18d213
Mohammad <sharifnewaz1996@gmail.com>
2021-11-14 01:25:35 -0800

    remove passing object and only render login

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,10 +1,5 @@
 
 let getLogin = async (req, res, _next) => {
-
-    let success_msg = req.flash('success_msg');
-    console.log(success_msg[0])
-    res.render('logIn', {
-        success_msg: success_msg[0]
-    });
+    res.render('logIn');
 }
 module.exports = { getLogin };
