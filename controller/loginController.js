3266b3580178d94f0b9a2326e1da2c3ed3f9e0f5
Mohammad <sharifnewaz1996@gmail.com>
2021-11-16 08:45:04 -0800

    add authentication funtionality and handle post request

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,5 +1,15 @@
+const passport = require('passport');
 
-let getLogin = async (req, res, _next) => {
-    res.render('logIn');
+module.exports = {
+    getLogin: async (_req, res, _next) => {
+        res.render('logIn');
+    },
+
+    postLogin: (req, res, next) => {
+        passport.authenticate('local', {
+            successRedirect: '/articles/api',
+            failureRedirect: '/auth/login',
+            failureFlash: true
+        })(req, res, next);
+    }
 }
-module.exports = { getLogin };
