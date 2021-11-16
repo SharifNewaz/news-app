20a55d0ce8c3827965608a5f758f800884b09404
Mohammad <sharifnewaz1996@gmail.com>
2021-11-16 08:53:07 -0800

    add articles/api route

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 /* GET home page. */
-router.get('/', function (req, res) {
-    res.render('articles');
-})
+router.route('/api')
+    .get(ensureAuthenticated, (req, res) => {
+        res.render('articles', {
+            name: req.user.uname
+        });
+    });
 
 module.exports = router;
