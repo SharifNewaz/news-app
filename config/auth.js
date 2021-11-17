530ab0569d96be211e69563d908f0ffa63110d5f
Mohammad <sharifnewaz1996@gmail.com>
2021-11-16 23:00:52 -0800

    add comments

--- config/auth.js
+++ config/auth.js
@@ -1,4 +1,7 @@
 module.exports = {
+    //if the req is authenticated then we return next to
+    //continue rendeering the expencted page bcz the user is authenticated
+    //if the req is not authenticated then we redirect to the login page
     ensureAuthenticated: (req, res, next) => {
         if (req.isAuthenticated()) {
             return next();
@@ -7,6 +10,9 @@ module.exports = {
         res.redirect('/auth/login');
     },
 
+    //if the req is not authenticated then we return next to
+    //continue rendering the expencted pages(login or signup)
+    //if the req is authenticated then we redirect to the article page
     forwardAuthenticated: (req, res, next) => {
         if (!req.isAuthenticated()) {
             return next();
