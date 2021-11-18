394e1238af6c8a8684b996e04ae862a30cd2cdba
Mohammad <sharifnewaz1996@gmail.com>
2021-11-17 22:33:17 -0800

    Destroy session and logout

--- controller/logoutController.js
+++ controller/logoutController.js
@@ -1,9 +1,20 @@
 module.exports = {
     getLogout: (req, res, next) => {
-        //passport middleware gives us the logout funtion
+        //passport middleware gives us the logout() funtion
+        //req.logout() only deletes the database users' info from session
+        //it does not remove thge session from the database
+        //On the other hand destroy functions destrys the sessions
+        //and removes it from the DB.
+        console.log(req.session)
         req.logout();
-        req.flash({ 'success_msg': 'You are logged out' });
-        res.redirect('/auth/login');
+        req.session.destroy((err) => {
+            if (err) throw err;
+            //clear coockies from the browser
+            res.clearCookie('connect.sid');
+            //send logout page instead of redirecting to a new page
+            //bcz redirection will make a new session insteantly
+            //right after destroying the session
+            res.render('logout');
+        });
     }
 }
-
