e4ecff53c7aa2b44511fb2d617690c5172ee6ffe
Mohammad <sharifnewaz1996@gmail.com>
2021-11-18 18:41:20 -0800

    add search article and  save button funtionality

--- controller/articlesController.js
+++ controller/articlesController.js
@@ -17,5 +17,33 @@ module.exports = {
         } catch (err) {
             console.log("error with querying docs using find()" + err)
         }
+    },
+
+    postUserArticles: async (req, res, next) => {
+        // In POST req 1, req.body will only contain one key value pair i.e searchtext
+        // However , In POST req 2 it has mutiple key value pairs in req.body 
+        // such as usersNames, userArticleTitle etc.
+        // Thus, check if length of key in req.body is 1, 
+        // then  search the title with API call+etc. 
+        // else we just insert user + article info in db
+        if (Object.keys(req.body).length === 1) {
+            let articleTitle = 'q=' + (req.body).articleTypes;
+            try {
+                let data = await newsFetch(articleTitle);
+                res.render('articles', {
+                    articles: data.articles,
+                    name: req.user.uname,
+                })
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            // -initialize newArticle
+            // -a single string name is converted 
+            // to array element(we do this bcz if the name is single string)
+            // then later array goes through each charecter rather
+            // than treating is as single string array
+            console.log("I'm in here", req.body)
+        }
     }
 }
