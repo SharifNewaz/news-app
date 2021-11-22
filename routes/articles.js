20a2f2572478c2fa8aa880cde1a012328a5a54e2
Mohammad <sharifnewaz1996@gmail.com>
2021-11-21 23:44:08 -0800

    add routes for save, delete and edit

--- routes/articles.js
+++ routes/articles.js
@@ -2,15 +2,19 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 const { getAllArticles, postUserArticles } = require('../controller/articles-controller');
-const { getSavedArticles } = require('../controller/saved-articles-controller');
+const { getSavedArticles, deleteSavedArticle, getEditArticle, putEditArticle } = require('../controller/saved-articles-controller');
 
 /* GET and POST news page. */
 router.route('/api')
     .get(ensureAuthenticated, getAllArticles)
     .post(ensureAuthenticated, postUserArticles);
 
-/* GET and POST saved-article page. */
-router.route('/saved-articles')
-    .get(ensureAuthenticated, getSavedArticles);
+/* GET and DELETE saved-article page. */
+router.get('/saved-articles', ensureAuthenticated, getSavedArticles);
+router.delete('/saved-articles/:id', deleteSavedArticle);
+
+router.post('/edit-article', ensureAuthenticated, getEditArticle);
+router.put('/edit-article/:id', putEditArticle);
+
 
 module.exports = router;
