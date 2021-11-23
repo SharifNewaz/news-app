96f9f701ecb3dbcdf0832e719b645c417d052cca
Mohammad <sharifnewaz1996@gmail.com>
2021-11-22 23:21:27 -0800

    add about endpoint

--- server.js
+++ server.js
@@ -68,6 +68,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/articles', articlesRouter);
+app.use('/about', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
