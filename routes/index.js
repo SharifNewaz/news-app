d04c807f1243de08c2a7b2b702977435d5012d23
Mohammad <sharifnewaz1996@gmail.com>
2021-11-22 23:23:38 -0800

    add router for about page rendering

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 /*localhost:3000/ redirect to -> /auth/login*/
 router.get('/', function (_req, res) {
     res.redirect('/auth/login');
+});
+
+router.get('/developer', ensureAuthenticated, function (req, res) {
+    res.render('about', {
+        name: req.user.uname
+    });
 });
 
 module.exports = router;
