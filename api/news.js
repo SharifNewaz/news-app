7e93891a69ff76c8257ef1ad0ccd1967370737a6
Mohammad <sharifnewaz1996@gmail.com>
2021-11-22 23:19:26 -0800

    add comments

--- api/news.js
+++ api/news.js
@@ -1,20 +1,19 @@
 const fetch = require('cross-fetch')
 require('dotenv').config();
 
+//fetch the data from api
+//if response is bad throw an error
+//store the json response in data and return it
 let newsFetch = async (articleTypes) => {
-    //link 
     let apiLink = `https://newsapi.org/v2/everything?${articleTypes}&apiKey=${process.env.API_KEY}`;
 
     try {
-        //fetch the data from api
         let response = await fetch(apiLink);
 
-        //if response is bad throw an error
         if (response.status >= 400) {
             throw new Error("Bad response from server");
         }
 
-        //store the json response in data and return it
         let data = await response.json();
         return data;
 
